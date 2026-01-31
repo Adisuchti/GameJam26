36104dd87eec9362a308faf2b2d78d46c59481e8
Erik <erik.steinacher@gmail.com>
2026-01-31 20:31:52 +0100

    Add UID file for intro3.ogv asset

--- scripts/startupscene.gd
+++ scripts/startupscene.gd
@@ -1,14 +1,39 @@
 extends Node2D
 
+@onready var video: VideoStreamPlayer = $Video
 
-# Called when the node enters the scene tree for the first time.
+const VIDEOS := [
+	"res://assets/intro1.ogv",
+	"res://assets/intro2.ogv",
+	"res://assets/intro3.ogv"
+]
+
 func _ready() -> void:
-	pass # Replace with function body.
+	video.visible = false
+	video.loop = false
+	video.autoplay = false
+
+	# Pick random video
+	var path: String = VIDEOS.pick_random()
+	video.stream = load(path)
+
+	# --- WARM-UP (prevents lag) ---
+	video.volume_db = -80
+	video.play()
+
+	await get_tree().process_frame
+
+	video.stop()
+	video.volume_db = 0
+
+	# --- ACTUAL PLAY ---
+	video.visible = true
+	video.play()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
+func _on_video_finished() -> void:
+	get_tree().change_scene_to_file("res://scenes/start/mainpage.tscn")
+
+
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/start/mainpage.tscn")
