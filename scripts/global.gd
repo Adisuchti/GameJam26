1e262a6b9fd0149d52957646720aaf39b090c97b
Erik <erik.steinacher@gmail.com>
2026-02-01 14:44:08 +0100

    Refactor game_over scene and update mainpage resources for improved UI elements

--- scripts/global.gd
+++ scripts/global.gd
@@ -74,15 +74,18 @@ signal item_picked_up(node: Node)
 signal enemy_detected_player(boolean: bool)
 
 
-# RUNS EVERY FRAME
-@warning_ignore("unused_parameter")
-func _process(delta: float) -> void:
+func _ready() -> void:
 	global.cap_picked_up.connect(_on_cap_picked_up)
 	global.cap_fell_off.connect(_on_cap_fell_off)
 	global.mask_lost_forever.connect(_on_mask_lost_forever)
 	global.mask_restored.connect(_on_mask_restored)
 	global.mask_hide.connect(_on_mask_hide)
 	global.mask_show.connect(_on_mask_show)
+
+# RUNS EVERY FRAME
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	pass
 
 
 func _on_mask_show() -> void:
@@ -103,9 +106,6 @@ func _on_mask_lost_forever() -> void:
 func _on_mask_restored() -> void:
 	mask_down = false
 
-# ON STARTUP
-func _ready() -> void:
-	pass
 
 
 # DEFINE FUNCTIONS HERE
