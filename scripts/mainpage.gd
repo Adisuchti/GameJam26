952c6a2c1f0359cf2427c1efe20b692e5725d561
Erik <erik.steinacher@gmail.com>
2026-02-01 11:31:08 +0100

    hotfix3

--- scripts/mainpage.gd
+++ scripts/mainpage.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/gameloop/tilemap.tscn")
+	get_tree().change_scene_to_file("res://scenes/gameloop/game.tscn")
 
 
 func _on_quit_pressed() -> void:
